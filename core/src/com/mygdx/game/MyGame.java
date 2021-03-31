4b06f0dae7e9908200ea51aa3fcc381dd4ffec72
Xxxy2 <ubysz.marcin@gmail.com>
2021-03-31 19:51:40 +0200

    zmiana tla, zmiana nazwy okna

--- core/src/com/mygdx/game/MyGame.java
+++ core/src/com/mygdx/game/MyGame.java
@@ -24,7 +24,7 @@ public class MyGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.741f, 0.874f, 0.976f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -11,7 +11,7 @@ public class DesktopLauncher {
 		config.resizable = false;
 		config.width = 900;
 		config.height = 600;
-		config.title = "Okno o konkretnym rozmiarze – https://kursjava.com";
+		config.title = "Projekt Java Tower Defence";
 
 		new LwjglApplication(new MyGame(), config);
 	}
